95928973186dc28041d6a86bcea59cfa8efdb32a
mustakarhu <15160022+mustakarhu@users.noreply.github.com>
2020-03-19 22:42:43 +0200

    add Jupyter notebook + update scrapper
    add placeholder for jupyter notebook
    add comments on main.py (scrapper of trump archive).

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-import csv
+from selenium import webdriver  # to scrape the data
+from selenium.webdriver.common.keys import Keys  # input keys for commands
+import time  # used for waiting periods
+import datetime
 
 # micro sleep for the site to load, sleep for the dataset to load
-u_sleep = 2
+u_sleep = 1
 sleep = 5
 
 if __name__ == '__main__':
 
     # url of the site where we want to fetch the data
     url = 'http://www.trumptwitterarchive.com/archive'
-    periods = [['01', 'Jan', Keys.ARROW_RIGHT, '2020'],
-               ['18', 'Mar', Keys.ARROW_RIGHT, '2020']]
+    # set the periods to which we will gather our data. This is a
+    # list of commands for the selenium date picker
+    today = datetime.date.today()
+    day = str(today.strftime('%d'))
+    month = str(today.strftime('%b'))
+    year = str(today.strftime('%Y'))
+
+    periods = [['01', 'Jan', Keys.ARROW_RIGHT, '2016'],
+               [day, month, Keys.ARROW_RIGHT, year]]
+
+    # uncheck the automatic retweets select '2'
     options = ['2']
     export_option = '3'  # '3' for csv '4' for json
 
     # set-up chrome webdriver
+    # select the options here
     web_options = webdriver.ChromeOptions()
     web_options.add_argument('incognito')
-    # options.add_argument('headless')
+    # web_options.add_argument('headless')
     browser = webdriver.Chrome(options=web_options)
     browser.get(url)
 
@@ -31,7 +41,7 @@ if __name__ == '__main__':
             dates.send_keys(elem)
             time.sleep(u_sleep)
 
-    # open options menu
+    # open options menu and select the options from the variable options defined above
     options_button = browser.find_element_by_xpath('//*[@id="options-button"]/button')
     options_button.click()
     for elem in options:
@@ -43,15 +53,18 @@ if __name__ == '__main__':
     export_button.click()
     export_format_button = browser.find_element_by_xpath(f'//*[@id="exports-button"]/ul/li[{export_option}]')
     export_format_button.click()
-    # need to wait info to load
+    # need to wait info to load especially for large datasets
     time.sleep(sleep)
+
+    # refresh if not loaded
     refresh = browser.find_element_by_xpath('//*[@id="export-box"]/div[2]/button[1]').click()
     time.sleep(sleep)
-
     content = browser.find_element_by_xpath('//*[@id="export-box"]/textarea').get_attribute('value')
-
-    t = (content.split('\n'))
-
+    # close the browser
+    browser.close()
+    
+    # breaking the  content into entries with split and save with utf8 encoding.
+    t = content.split('\n')
     with open('tweets.csv', 'w', encoding='utf-8') as f:
         for entry in t:
             f.write(f'{entry}\n')
