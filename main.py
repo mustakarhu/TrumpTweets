748d9f44e8288ce429606eb3a500709b38bf7721
mustakarhu <15160022+mustakarhu@users.noreply.github.com>
2020-03-21 20:19:11 +0200

    update code for scraper
    add headless mode and webdriver options

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import time  # used for waiting periods
 import datetime
 
 # micro sleep for the site to load, sleep for the dataset to load
-u_sleep = 1
-sleep = 5
+u_sleep = 0.5
+sleep = 1
+PRINT = True
 
 if __name__ == '__main__':
 
@@ -26,14 +27,22 @@ if __name__ == '__main__':
 
     # set-up chrome webdriver
     # select the options here
+    if PRINT:
+        print('Setting up web scraper')
     web_options = webdriver.ChromeOptions()
     web_options.add_argument('incognito')
-    # web_options.add_argument('headless')
+    web_options.add_argument('headless')
+    web_options.add_argument('disable-gpu')
+    web_options.add_argument('window-size=1920,1080')
     browser = webdriver.Chrome(options=web_options)
+    if PRINT:
+        print('retrieving website data')
     browser.get(url)
 
     time.sleep(sleep)
     # set the start and end periods
+    if PRINT:
+        print('Setting up filter parameters')
     for d in range(2):
         dates = browser.find_element_by_xpath(f'//*[@id="control-dates"]/input[{d+1}]')
         for elem in periods[d]:
@@ -54,14 +63,19 @@ if __name__ == '__main__':
     export_format_button.click()
     # need to wait info to load especially for large datasets
     time.sleep(sleep)
+    if PRINT:
+        print('collecting data')
 
     # refresh if not loaded
-    refresh = browser.find_element_by_xpath('//*[@id="export-box"]/div[2]/button[1]').click()
-    time.sleep(sleep)
+    # refresh = browser.find_element_by_xpath('//*[@id="export-box"]/div[2]/button[1]').click()
+    # time.sleep(sleep)
     content = browser.find_element_by_xpath('//*[@id="export-box"]/textarea').get_attribute('value')
     # close the browser
     browser.close()
-    
+
+    if PRINT:
+        print('write on file')
+
     # breaking the  content into entries with split and save with utf8 encoding.
     t = content.split('\n')
     with open('tweets.csv', 'w', encoding='utf-8') as f:
